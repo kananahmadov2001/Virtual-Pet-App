c6aac0d75a78ead671327f41d613c02115555fe1
Kanan Ahmadov <a.kanan@wustl.edu>
2024-09-17 14:29:19 -0700

    Initial Commit

---
+++ KananAhmadov-Lab2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KananAhmadov-Lab2
+//
+//  Created by Ahmadov, Kanan on 9/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
