d12eed536fb006286c8d92ebde9736ce43c265c3
Kanan Ahmadov <135070652+kananahmadov2001@users.noreply.github.com>
2024-09-23 16:18:58 -0500

    Update Pet.swift

--- KananAhmadov-Lab2/Pet.swift
+++ KananAhmadov-Lab2/Pet.swift
@@ -14,6 +14,7 @@ class Pet {
     var foodLevel: Int
     var timesPlayed: Int
     var timesFed: Int
+    var consecutiveHappyInteractions: Int = 0
 
     init(name: String, image: String, happiness: Int = 0,
          foodLevel: Int = 0, timesPlayed: Int = 0,
@@ -26,11 +27,29 @@ class Pet {
         self.timesPlayed = timesPlayed
         self.timesFed = timesFed
     }
-   
+    
+    // creative portion
+    var mood: String {
+        if happiness >= 7 && foodLevel >= 5 {
+            consecutiveHappyInteractions += 1
+            return "Happy"
+        } else if happiness >= 4 && foodLevel >= 3 {
+            consecutiveHappyInteractions = 0
+            return "Neutral"
+        } else {
+            consecutiveHappyInteractions = 0
+            return "Sad"
+        }
+    }
+    
     // increasing happiness and decreasing food level if the pet can be played with
     func play() -> Bool {
         if foodLevel > 0 {
-            happiness += 1
+            if mood == "Happy" {
+                happiness += 2 // bonus for playing while happy
+            } else {
+                happiness += 1
+            }
             foodLevel -= 1
             timesPlayed += 1
             return true
